77c373dba1fba39421156506ebdf37cc032859db
Michael Chappel <michael.e.chappel@gmail.com>
2023-07-15 00:44:45 -0400

    Cleaned up boilerplate and started audio testing
    Co-authored-by: WholeStackDev <WholeStackDev@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,41 +1,31 @@
 require("dotenv").config();
-const fs = require("node:fs");
-const path = require("node:path");
 const token = process.env.DISCORD_TOKEN;
 
 // Require the necessary discord.js classes
-const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
+const { Client, Events, GatewayIntentBits, Collection } = require("discord.js");
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
-client.commands = new Collection();
-const foldersPath = path.join(__dirname, "commands");
-const commandFolders = fs.readdirSync(foldersPath);
+client.on(Events.MessageCreate, async (message) => {
+	if (message.author.bot) return;
+	console.log("Message received: ", message.content);
 
-for (const folder of commandFolders) {
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".js"));
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
-		if ("data" in command && "execute" in command) {
-			client.commands.set(command.data.name, command);
-		} else {
-			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-		}
+	if (message.attachments.size > 0) {
+		console.log("Message has attachments");
+		const audio = message.attachments.first();
+		await message.reply({
+			content: "Here is your audio file!",
+			embeds: [
+				{
+					url: audio.url,
+					title: "Audio File",
+				},
+			],
+		});
 	}
-}
+});
 
-const eventsPath = path.join(__dirname, "events");
-const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith(".js"));
-
-for (const file of eventFiles) {
-	const filePath = path.join(eventsPath, file);
-	const event = require(filePath);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
-}
+client.once(Events.ClientReady, (c) => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
 
 client.login(token);
